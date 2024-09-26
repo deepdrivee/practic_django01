b20fca2d0ace15413f54774a4c6a02b391d796e7
aniket <pawaraniket303@gmail.com>
2024-09-26 01:48:54 -0700

    models_done

--- studentproject/app/admin.py
+++ studentproject/app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import student
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = [
+        "studentid",
+        "name",
+        "gender",
+        "photo",
+    ]
+
+admin.site.register(student,StudentAdmin)
--- studentproject/app/models.py
+++ studentproject/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    studentid =models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    type = [("male","male"),("female","female")]
+    gender = models.CharField(max_length=50,choices=type,default=None)
+    photo = models.ImageField(upload_to="studentImg")
